73734cb2cdeaecbe0266c3fac5d46c59abf557ed
Aslıhan Nurcan <aslinagi.95@outlook.com>
2018-08-26 23:40:12 +0300

    Update kayitislemleri_js.js

--- kayitislemleri/static/kayitislemleri_js.js
+++ kayitislemleri/static/kayitislemleri_js.js
@@ -14,5 +14,32 @@ $(document).ready(function() {
             }
         });
        });
+       $( "#positionSelecte" ).change(function() {
+           var position;
+           $( "select option:selected" ).each(function() {
+               position =$( this ).val();
+                $.ajax({
+                   type:'get',
+                   url: '/personel/filter/'+position,
+                   contentType: 'application/json',
+                });
+           });
+
+       });
+
+      // $( "#positionSelecte" ).change(function() {
+       //     var position;
+         //   $( "select option:selected" ).each(function() {
+           //     position = $( this ).text();
+             //   $("td").each(function(){
+               //     if($(this).text()!=position)
+                 //       $(this).parent().addClass('hide')
+                  //  else
+                    //    $(this).parent().removeClass('hide')
+              //  })
+           // });
+
+     //  });
+
 
 });
